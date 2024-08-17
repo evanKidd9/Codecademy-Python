9eff72a0033704b8e19c0ccb9d084ec8b650d2a5
TheBigShaq <93743430+TheBigShaq@users.noreply.github.com>
2024-08-17 18:47:38 -0400

    Create initials.py

---
+++ initials.py
@@ -0,0 +1,24 @@
+#My name is Evan!
+#I have a passion for cybersecurity!
+
+first_initial = """
+EEEEE
+E
+E
+EEE
+E
+E
+EEEEE
+"""
+last_initial = """
+K   K
+K  K
+K K
+KK
+K K
+K  K
+K   K
+"""
+
+print(first_initial)
+print(last_initial)
